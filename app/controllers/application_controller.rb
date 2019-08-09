e43e2dace501af14db6085fb382bed7f360fbc56
Ashna Shah-Grover <as19@williams.edu>
2019-08-09 10:48:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,46 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params[:article])
+    redirect "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    find_article
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    find_article
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    find_article
+    @article.update(params[:article])
+    redirect "articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    find_article
+    @article.destroy
+    redirect "/articles"
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+
 end
